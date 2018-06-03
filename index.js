e789218ed9237917e209f164452deaa38a64952f
Vadim Samarin <dezivert@gmail.com>
2018-06-03 05:35:58 +0300

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const createHandler = require('./api-entry');
+
+module.exports = (mapping, options, cb) => {
+  const API = {};
+  cb = cb || options;
+
+  Object.keys(mapping).forEach(key => {
+    let path = mapping[key];
+    let queriesDescriptions;
+    
+    if (typeof path === 'array') {
+      path = path[0];
+      queriesDescriptions = path[1];
+    }
+    
+    const entry = createHandler(path, queriesDescriptions);
+    
+    API[key] = (...args) => {
+      const requestOptions = entry(...args);      
+      return cb(requestOptions);
+    };
+  });
+
+  return API;
+};
+
+module.exports.createHandler = createHandler;
